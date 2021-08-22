af67e1a2543bbf54f4222682ad24a85ebe7158dd
Joelito1031 <joelitocaorte1031@gmail.com>
2021-08-22 18:01:55 +0800

    Aug 22 6:01PM

--- dummy.php
+++ dummy.php
@@ -1,21 +1,27 @@
 <?php
-
 $server = 'localhost';
 $username = 'root';
 $password = '';
 $dbname = 'OCQMS';
-
-$plateno = "HVM-601";
-$route = "Valencia";
-$capacity = 10;
+$plateno = 'HVM-601';
+$route = 'valencia';
+$capacity = 20;
 
 try{
-  $conn = new PDO("mysql:host=$server;dbname=OCQMS", $username, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO registered_vehicles(PlateNo, Route, Capacity) VALUES('$plateno', '$route', '$capacity')";
-  $conn->exec($sql);
-  echo "Connection successful";
-}
-catch(PDOException $e){
-  echo "Connection failed: " . $e->getMessage();
-}
+  $connection = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $insert_query = "INSERT INTO registered_vehicles(PlateNo, Route, Capacity) VALUES('$plateno', '$route', '$capacity')";
+  $select_query = "SELECT * FROM registered_vehicles WHERE PlateNo = '$plateno'";
+
+  $num_row = $connection->query($select_query);
+  $row_count = $num_row->fetchColumn();
+
+  if($row_count > 0){
+    echo "Count is greater than 1";
+  }
+  else{
+    echo "Count is is 0";
+  }
+  }catch(PDOException $e){
+    echo 'Error:' . ' ' . $e->getMessage();
+  }
--- process.php
+++ process.php
@@ -16,25 +16,41 @@ if((isset($_POST['fname']) && !empty(trim($_POST['fname']))) && (isset($_POST['m
 
   try{
     $connection = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = "INSERT INTO registered_vehicles(PlateNo, Route, Capacity) VALUES('$plateno', '$route', '$capacity')";
-    $connection->exec($query);
-    echo "Data inserted";
-    ob_start();
-    $name = $_POST['fname'] . ' ' . $_POST['mname'] . ' ' . $_POST['lname'];
-    $capacity = (int) $_POST['capacity'];
-    $data = array($_POST['plateno'] => array("operator" => $_POST['fname'] . " " . $_POST['mname'] . " " . $_POST['lname'],
-            "contact_num" => $_POST['cnum'], "route" => $_POST['route'], "capacity" => $capacity, "passengers" => 0, "queuing" => false));
-    $vehicles_json = file_get_contents('./vehicles/vehicles.json');
-    $vehicles_data = json_decode($vehicles_json, true);
-    array_push($vehicles_data, $data);
-    $data_json = json_encode($vehicles_data);
-    $vehicles_file = fopen('./vehicles/vehicles.json', 'w');
-    fwrite($vehicles_file, $data_json);
-    fclose($vehicles_file);
-    QRcode::png($_POST['plateno'], './qrs/' . $_POST['plateno'] . '.png', QR_ECLEVEL_L, 4, 10);
-    ob_end_clean();
-    header('Location: ./index.html?qr=' . $_POST['plateno']);
+
+    $select_query = "SELECT * FROM registered_vehicles WHERE PlateNo = '$plateno'";
+    $num_row = $connection->query($select_query);
+    $row_count = $num_row->fetchColumn();
+
+    if($row_count > 0){
+      echo "Vehicle already registered";
+    }
+    else{
+      $insert_query = "INSERT INTO registered_vehicles(PlateNo, Route, Capacity) VALUES('$plateno', '$route', '$capacity')";
+
+      $connection->exec($insert_query);
+      ob_start();
+
+      $name = $_POST['fname'] . ' ' . $_POST['mname'] . ' ' . $_POST['lname'];
+      $capacity = (int) $_POST['capacity'];
+      $data = array($_POST['plateno'] => array("operator" => $_POST['fname'] . " " . $_POST['mname'] . " " . $_POST['lname'],
+              "contact_num" => $_POST['cnum'], "route" => $_POST['route'], "capacity" => $capacity, "passengers" => 0, "queuing" => false));
+      $vehicles_json = file_get_contents('./vehicles/vehicles.json');
+      $vehicles_data = json_decode($vehicles_json, true);
+
+      array_push($vehicles_data, $data);
+
+      $data_json = json_encode($vehicles_data);
+      $vehicles_file = fopen('./vehicles/vehicles.json', 'w');
+
+      fwrite($vehicles_file, $data_json);
+      fclose($vehicles_file);
+      QRcode::png($_POST['plateno'], './qrs/' . $_POST['plateno'] . '.png', QR_ECLEVEL_L, 4, 10);
+      ob_end_clean();
+      header('Location: ./index.html?qr=' . $_POST['plateno']);
+      echo "Vehicle registered";
+    }
   }catch(PDOException $e){
     echo 'Error:' . ' ' . $e->getMessage();
   }
