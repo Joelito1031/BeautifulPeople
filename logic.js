277adc5a8e4ed7ea6368617a858f5c2d2bf6e0e8
Joelito1031 <joelitocaorte1031@gmail.com>
2021-08-26 00:05:46 +0800

    Some major changes

--- logic.js
+++ logic.js
@@ -1,40 +1,72 @@
 'use strict';
 
-let span = document.querySelector('.a');
-let span_value = getComputedStyle(span);
+let a_one = document.querySelector('.subfield-1-1');
+let a_two = document.querySelector('.subfield-1-2');
+let b_one = document.querySelector('.subfield-2-1');
+let b_two = document.querySelector('.subfield-2-2');
+let span_a = document.querySelector('.a');
+let span_b = document.querySelector('.b');
+let span_value_a = getComputedStyle(span_a);
+let span_value_b = getComputedStyle(span_b);
 
 function checkSize(){
   if(window.innerWidth <= '609'){
-    span.style.width = '50px';
-    document.getElementById('show-btn').style.display = 'none';
-    document.getElementById('ttl').style.display = 'none';
-    document.getElementById('ttl-pic').style.display = 'inline-block';
+    span_a.style.width = '50px';
+    span_b.style.width = '50px';
+    document.querySelector('.show-btn').style.display = 'none';
+    document.querySelector('.one-ttl').style.display = 'none';
+    document.querySelector('.two-ttl').style.display = 'none';
+    document.querySelector('.ttl-pic-one').style.display = 'inline-block';
+    document.querySelector('.ttl-pic-two').style.display = 'inline-block';
   }
   else{
-    span.style.width = '200px';
-    document.getElementById('show-btn').style.display = 'inline-block';
-    document.getElementById('ttl').style.display = 'inline';
-    document.getElementById('ttl-pic').style.display = 'none';
+    span_a.style.width = '200px';
+    span_b.style.width = '200px';
+    document.querySelector('.show-btn').style.display = 'inline-block';
+    document.querySelector('.one-ttl').style.display = 'inline';
+    document.querySelector('.two-ttl').style.display = 'inline';
+    document.querySelector('.ttl-pic-one').style.display = 'none';
+    document.querySelector('.ttl-pic-two').style.display = 'none';
   }
 }
 
 window.addEventListener('resize', checkSize);
 
 function showNav(){
-  if(span_value.width == '200px'){
-    span.style.width = '50px';
-    document.getElementById('ttl').style.display = 'none';
-    document.getElementById('ttl-pic').style.display = 'inline-block';
+  if(span_value_a.width == '200px'){
+    span_a.style.width = '50px';
+    span_b.style.width = '50px';
+    document.querySelector('.one-ttl').style.display = 'none';
+    document.querySelector('.two-ttl').style.display = 'none';
+    document.querySelector('.ttl-pic-one').style.display = 'inline-block';
+    document.querySelector('.ttl-pic-two').style.display = 'inline-block';
   }
   else{
-    span.style.width = '200px';
-    document.getElementById('ttl').style.display = 'inline';
-    document.getElementById('ttl-pic').style.display = 'none';
+    span_a.style.width = '200px';
+    span_b.style.width = '200px';
+    document.querySelector('.one-ttl').style.display = 'inline';
+    document.querySelector('.two-ttl').style.display = 'inline';
+    document.querySelector('.ttl-pic-one').style.display = 'none';
+    document.querySelector('.ttl-pic-two').style.display = 'none';
   }
 }
 
+function showA(){
+  a_one.style.display = 'block';
+  a_two.style.display = 'block';
+  b_one.style.display = 'none';
+  b_two.style.display = 'none';
+}
+
+function showB(){
+  b_one.style.display = 'block';
+  b_two.style.display = 'block';
+  a_one.style.display = 'none';
+  a_two.style.display = 'none';
+}
+
 function savePDF(){
-  var element = document.getElementById('for-print');
+  var element = document.getElementById('vehicle-for-print');
   html2pdf(element, {
     filename: parameter + '.pdf'
   });
@@ -43,6 +75,8 @@ function savePDF(){
 const urlString = window.location.search;
 const urlParams = new URLSearchParams(urlString);
 const parameter = urlParams.get('qr');
+const parameter_passenger = urlParams.get('qr_passenger');
 
-document.getElementById('plateno').innerHTML = parameter;
-parameter == null ? document.getElementById('qrimage').src = './qrs/loading.gif' : document.getElementById('qrimage').src = './qrs/' + parameter + '.png';
+document.getElementById('vehicle-plateno').innerHTML = parameter;
+parameter == null ? document.getElementById('vehicle-qrimage').src = './qrs/loading.gif' : document.getElementById('vehicle-qrimage').src = './qrs/' + parameter + '.png';
+parameter_passenger == null ? document.getElementById('passenger-qrimage').src = './qrs/loading.gif' : document.getElementById('passenger-qrimage').src = './qrs/' + parameter + '.png';
---
+++ passenger_process.php
@@ -0,0 +1,23 @@
+#passenger-for-print{
+  box-shadow: -5px 5px 10px black;
+  border-radius: 10px;
+  width: 272px;
+  height: 334px;
+  position: relative;
+  background-image: url('./background.JPG');
+}
+
+#passenger-qrimage{
+  border-radius: 5px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+#passenger-plateno{
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  top: 60%;
+}
